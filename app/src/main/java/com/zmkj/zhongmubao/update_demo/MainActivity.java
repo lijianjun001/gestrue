557067d9246ea6fd274586d4886b0fdfed0d5a41
lijianjun <1455404626@qq.com>
2018-05-25 14:16:39 +0800

    修改问题

--- app/src/main/java/com/zmkj/zhongmubao/update_demo/MainActivity.java
+++ app/src/main/java/com/zmkj/zhongmubao/update_demo/MainActivity.java
@@ -3,11 +3,14 @@ package com.zmkj.zhongmubao.update_demo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.zmkj.gesture.GestureContentView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GestureContentView gestureContentView;
     }
 }
